d578e89f3538c6c90ea5c484c6ff5f8cd1cf7120
FayzullayevAbbos <abbosfayzullyev202@gmail.com>
2024-06-24 22:26:46 +0500

    router init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-
+import {
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
 import "./App.css";
+import Home from "./pages/home/Home";
 
 function App() {
-
-  return <div className="">App</div>
+  const router = createBrowserRouter([
+    {
+      element: <Home />,
+      path: "/",
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
